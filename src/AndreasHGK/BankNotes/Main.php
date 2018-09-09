5f9a3e6727649d30a408b6c963cc7d8798949cf1
AndreasHGK <36563463+AndreasHGK@users.noreply.github.com>
2018-09-09 13:36:20 +0200

    Lots of changes
    ! THIS IS NOT FULLY TESTED YET !
    
    The notes are now reworked, but might be reworked even more.
     - They will now store data using NBT.
     - They store their value, creator, version (of the notes) and an isValidNote bool
     - This bool will be usefull in the future for devalidating notes (EG if you want to reset economy or use a note as example so noone can claim it
    
    You can now configure the plugin:
     - You can change the lore, ID and name of banknotes (and the older ones will keep on working
    
    Other plugin changes:
     - /banknote has been removed
     - /banknotes will be set aside for future admin commands
     - /withdraw will replace /banknote (to create notes
     - The plugin will no displays unneeded "enabled" or "disabled" messages (pocketmine already does this)

--- src/AndreasHGK/BankNotes/Main.php
+++ src/AndreasHGK/BankNotes/Main.php
@@ -18,10 +18,38 @@ use onebone\economyapi\EconomyAPI;
 use pocketmine\utils\TextFormat as C;
 
 class Main extends PluginBase implements Listener{
+	
+	public $NoteVersion = 2.0;
+	private $cfg;
 
 	public function onEnable() : void{
-		$this->getLogger()->info("enabled!");
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+		$this->cfg = $this->getConfig()->getAll();
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onDisable() : void{
+        $this->config->save();
+    }
+	
+	public function replaceVars($str, array $vars) : string{
+        foreach($vars as $key => $value){
+            $str = str_replace("{" . $key . "}", $value, $str);
+        }
+        return $str;
+    }
+	
+	public function getNameConfig(){
+		return TextFormat::colorize($this->replaceVars($this->cfg["note-name"], array(
+			"VALUE" => $amount,
+			"CREATOR" => $player)));
+	}
+	
+	public function getLoreConfig(){
+		return TextFormat::colorize($this->replaceVars($this->cfg["note-lore"], array(
+			"VALUE" => $amount,
+			"CREATOR" => $player)));
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
@@ -34,8 +62,12 @@ class Main extends PluginBase implements Listener{
 		switch(strtolower($command->getName())){
 			
 			case "banknotes":
-			case "banknote":
+			
+			return true;
+			break;
+			
 			case "note":
+			case "withdraw":
 				
 				#check if player used arguments
 				if(empty($args[0])){
@@ -54,11 +86,13 @@ class Main extends PluginBase implements Listener{
 					#make and give the custom bank note and reduce playermoney
 					EconomyAPI::getInstance()->reduceMoney($player, $amount);
 					$note = Item::get(339, 0, 1);
-					$note->setCustomName(C::RESET.C::YELLOW."$".C::GOLD.$amount.C::YELLOW." note");
-					$note->setLore([
-					C::RESET.C::DARK_RED."Right-Click ".C::RED."to claim this note",
-					C::RESET.C::RED.$amount
-					]);
+					$note->setCustomName($this->getNameConfig());
+					$note->setLore($this->getLoreConfig());
+					$nbt = $note->getNamedTag();
+					$nbt->setTag(new ByteTag("IsValidNote", true));
+					$nbt->setTag(new IntTag("NoteVersion", $this->NoteVersion));
+					$nbt->setTag(new IntTag("NoteValue", $amount));
+					$nbt->setTag(new StringTag("Creator", $player));
 					$sender->getInventory()->addItem($note);
 					$sender->sendMessage(C::GREEN.C::BOLD."Success! ".C::RESET.C::GRAY."a $".$amount." note was given.");
 					return true;
@@ -81,27 +115,27 @@ class Main extends PluginBase implements Listener{
 			$inv = $sender->getInventory();
 			$hand = $inv->getItemInHand();
 			$lore = $hand->getlore();
-			if (!empty($lore)) {
-			if(C::clean($lore[0]) == 'Right-Click to claim this note'){
-				$dep = (int)C::clean($lore[1]);
+			if ($nbt->hasTag("IsValidNote", ByteTag::class) && $nbt->getTagValue("IsValidNote") == true) {
+			if($nbt->hasTag("NoteVersion", IntTag::class) && $nbt->getTagValue("NoteVersion") == $this->NoteVersion){
+				$dep = $nbt->getTagValue("NoteValue");
 				EconomyAPI::getInstance()->addMoney($player, $dep);
 				$hand->setCount($hand->getCount() - 1);
 				$inv->setItemInHand($hand);
 				$sender->sendMessage(C::GREEN.C::BOLD."Success! ".C::RESET.C::GRAY."you deposited $".$dep." to your account.");
 				return true;
 			} else {
-				$sender->sendMessage(C::RED.C::BOLD."Error! ".C::RESET.C::GRAY."you must be holding a bank note.");
+				$p->sendMessage(C::RED.C::BOLD."Error: ".C::RESET.C::GRAY."this is an outdated note.");
 				return true;
 			}
-			} else {
-				$sender->sendMessage(C::RED.C::BOLD."Error! ".C::RESET.C::GRAY."you must be holding a bank note.");
-				return true;
+			}else{
+			$p->sendMessage(C::RED.C::BOLD."Error: ".C::RESET.C::GRAY."this note is no longer valid.");
+			return true;
 			}
 			break;
 			
 			default:
 			return false;
-	}
+		} return false;
 	}
 	
 	#Thanks to JackMD for providing help with this part!
@@ -110,9 +144,10 @@ class Main extends PluginBase implements Listener{
 		$name = $p->getName();
 		$inv = $p->getInventory();
 		$hand = $inv->getItemInHand();
-		$lore = $hand->getlore();
-		if (!empty($lore)) {
-			if(C::clean($lore[0]) == 'Right-Click to claim this note'){
+		$nbt = $hand->getNamedTag();
+		if ($nbt->hasTag("IsValidNote", ByteTag::class) && $nbt->getTagValue("IsValidNote") == true) {
+			if($nbt->hasTag("NoteVersion", IntTag::class) && $nbt->getTagValue("NoteVersion") == $this->NoteVersion){
+				#check to see on which block the player claims the note
 				switch($event->getBlock()->getName()){
 					
 				case "Item Frame":
@@ -134,11 +169,10 @@ class Main extends PluginBase implements Listener{
 				case "Trapped Chest":
 				case "Wooden Trapdoor":
 				case "Bed":
-					#$p->sendMessage(C::RED.C::BOLD."Error: ".C::RESET.C::GRAY."you are not allowed to place this item in an item frame.");
-					#$event->setCancelled();
 					break;
+					
 				default:
-				$dep = (int)C::clean($lore[1]);
+				$dep = $nbt->getTagValue("NoteValue");
 				EconomyAPI::getInstance()->addMoney($name, $dep);
 				$hand->setCount($hand->getCount() - 1);
 				$inv->setItemInHand($hand);
@@ -146,11 +180,9 @@ class Main extends PluginBase implements Listener{
 				break;
 				}
 				return;
+			}else{
+				$p->sendMessage(C::RED.C::BOLD."Error: ".C::RESET.C::GRAY."this is an outdated note.");
 			}
-	}
-	}
-	
-	public function onDisable() : void{
-		$this->getLogger()->info("disabled!");
+		}
 	}
 }
